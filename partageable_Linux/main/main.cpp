8445adb273210e0c80f8a94dc37dc1dd29f03f91
Deeplygends <peeren.pro@gmail.com>
2020-03-12 11:51:39 +0100

    ex1 factoring function + casting parameter in string

--- partageable_Linux/main/main.cpp
+++ partageable_Linux/main/main.cpp
@@ -4,38 +4,8 @@
 #include <stdio.h>
 #include "string.h"
 
-static int function1(int a, int b)
-{
-	int (*func)(int,int);
-	int t = 0;
-	void *handle;
-        // Ouverture de la bibliothèque
-        handle = dlopen("./libComposant1.so", RTLD_LAZY);
-        if(handle == NULL)
-        {
-                printf("erreur dlopen : %s\n", dlerror());
-                exit(EXIT_FAILURE);
-        }
 
-        // Chargement de la fonction "func"
-        *(void **) (&func) = dlsym(handle, "composant1");
-        if (func == NULL)
-        {
-		printf("Error occured");
-                printf("erreur dlsym : %s\n", dlerror());
-                dlclose(handle);
-                exit(EXIT_FAILURE);
-        }
-
-        // Exécution de la fonction "func"
-        t = func(a,b);
-
-        // Fermeture de la bibliothèque
-        dlclose(handle);
-
-	return t;
-}
-static int function2(int a, int b, std::string compo)
+static int operation(int a, int b, std::string compo)
 {
         int (*func)(int,int);
         int t = 0;
@@ -100,16 +70,17 @@ int main(int argc, char ** argv)
         std::cout << argc << " " << argv[1];
         if(argc == 2)
         {
-                std::cout << std::endl << "In the loop";
+                std::string arg1(argv[1]);
+                std::cout << std::endl << "In the loop " << arg1;
                 if(argv[1] == "Composant1")
                 {
                         std::cout << std::endl << "into component 1";
-                        valeur = function2(data1, data2, argv[1]);
+                        valeur = operation(data1, data2, arg1);
                 }
                 else if(argv[1] == "Composant2")
                 {
                         std::cout << std::endl << "into component 2";
-                        valeur = function2(data1, data2, argv[1]);
+                        valeur = operation(data1, data2, arg1);
                 }
         }
 
